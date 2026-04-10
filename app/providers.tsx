26696e89f4e090e2576b58f1b1765ac64f8292e6
Cezary Makowski <cezary.makowski96@gmail.com>
2026-04-10 20:16:31 +0200

    deleted sessionProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,14 +2,11 @@
 
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SessionProvider } from "next-auth/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
